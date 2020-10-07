c0cb54ea5153124ef3599574d28a6b33ac84079b
Samuel Santos <csamuelssm@gmail.com>
2020-10-07 15:55:12 -0300

    melhorias na física

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from sprites import *
 from constants import *
 from terrain_generator import *
 
+import numpy as np
+
 pygame.init()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -20,18 +22,31 @@ ball_group.add(ball)
 
 running = True
 clock = pygame.time.Clock()
+
+moving = True
 while running:
     clock.tick(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print("Espaço!")
+                ball.rect.move_ip(10, 5)
+                ball.f_r = np.add(ball.f_r, np.array([20, -20]))
+                moving = True
+                #ball_group.update()
 
     terrain_group.draw(screen)
     pygame.draw.polygon(screen, BROWN_SAND, terrain.points)
 
     if not pygame.sprite.groupcollide(ball_group, terrain_group, False, False,
                                       pygame.sprite.collide_mask):
-        ball_group.update()
+        moving = True
+    else:
+        moving = False
+
+    ball.update(moving)
 
     ball_group.draw(screen)
     pygame.display.update()
--- sprites.py
+++ sprites.py
@@ -3,6 +3,10 @@ import random
 import math
 from constants import *
 
+import numpy as np #importando o numpy pra fazer operações com vetores
+#para instalar ele é só fazer:
+#python -m pip install numpy
+
 class Ball(pygame.sprite.Sprite):
 
     def __init__(self):
@@ -13,8 +17,18 @@ class Ball(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 100
         self.rect.y = -500
-        self.vel = 0
+        #força resultante agindo na bolinha:
+        #como o jogo é em 2d, a força é um vetor bidimensional
+        #inicializando ela em 0
+        self.f_r = np.zeros((1, 2))
+        self.m = MASSA
 
-    def update(self):
-        self.vel += GRAVIDADE_TERRA/40
-        self.rect.bottom += self.vel
+    def update(self, moving):
+        if moving == True:
+            self.f_r = np.subtract(
+                np.subtract(self.f_r, self.m * GRAVIDADE_TERRA), (C_A_ESFERA_LISA * np.power(self.f_r, 2))
+                )
+            print(self.f_r[0, 1])
+            #self.rect.bottom += self.vel[0, 1]
+            self.rect.move_ip(self.f_r[0, 0], self.f_r[0, 1])
+        
