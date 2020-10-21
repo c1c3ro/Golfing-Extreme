baaf1542fec178b82617199bd28e9a5b9e06fdd4
Thiago Carlos Silva Pereira <49200148+thcls@users.noreply.github.com>
2020-10-21 16:41:40 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,40 +2,69 @@ import pygame
 from sprites import *
 from constants import *
 from terrain_generator import *
+from menu import *
 import math
+import constants
 
 vec = pygame.math.Vector2
 
 pygame.init()
 
+mode = EARTH_MODE
+
+ball_grav = BALL_GRAV
+
+# JOGO
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Golf Extreme")
 icon = pygame.image.load('golf-ball8.png')
 pygame.display.set_icon(icon)
+flag = pygame.image.load('flag.png')
+score = 1
+score_font = pygame.font.Font("freesansbold.ttf", 10)
 
-terrain = TerrainGenerator(WIDTH, HEIGHT, 8, (200, 320))
+# SONS
+background_song = pygame.mixer.Sound('sky_loop.wav')
+golf_hit = pygame.mixer.Sound('golf_ball.wav')
+
+# TERRENO
+terrain = TerrainGenerator(WIDTH, HEIGHT, 8, (200, 320), mode)
 terrain_group = pygame.sprite.Group()
 terrain_group.add(terrain)
 
-ball = Ball()
+# BOLA
+ball = Ball(mode, terrain.X[1] + 10)
 ball_group = pygame.sprite.Group()
 ball_group.add(ball)
 
+menu = Menu()
+
 running = True
 on_sand = False
+on_hole = 0
 
 mouse_old = False
 mouse_curr = False
 
 clock = pygame.time.Clock()
 
+running = menu.initial_menu(screen)
+
+background_song.play(-1)
+background_song.set_volume(0.4)
+
 while running:
-    clock.tick(30)
+    clock.tick(25)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill(BROWN_SKY)
+    if mode == EARTH_MODE:
+        screen.fill(BROWN_SKY)
+    elif mode == MARS_MODE:
+        screen.fill(SKY_MARS)
+    elif mode == MOON_MODE:
+        screen.fill(SKY_MOON)
 
     if pygame.sprite.groupcollide(ball_group, terrain_group, False, False, pygame.sprite.collide_mask):
         div_index = terrain.getDiv(ball.rect.x)
@@ -54,7 +83,7 @@ while running:
         angle_sin = math.sin(math.radians(angle + 180))
         angle_cos = math.cos(math.radians(angle + 180))
         #print(math.degrees(angle))
-        #print("Angulo: {}\nSeno: {}\nCoseno: {}".format(angle, math.sin(math.radians(angle)), math.cos(math.radians(angle))))
+        print("Angulo: {}\nSeno: {}\nCoseno: {}".format(angle, math.sin(math.radians(angle)), math.cos(math.radians(angle))))
         #agora eu vou inverter o vetor de velocidade da bola com relação à normal do plano
         #esse vetor vai ser o quique da bola
         bounce_vec = ball.vel.reflect(normal_vec)
@@ -63,15 +92,21 @@ while running:
         ball.vel = vec(0, 0)
         ball.vel += bounce_vec
         #aqui é onde acontece a física do plano inclinado:
-        plano_inclinado = CONSTANTE_PLANO * BALL_GRAV * angle_sin
-        if ball.vel.x - plano_inclinado >= COEFICIENTE_ATRITO_ESTATICO:
-            ball.vel.x -= plano_inclinado
-        if terrain.onHole(ball.rect.x):
+        #plano_inclinado = CONSTANTE_PLANO * ball_grav * angle_sin
+        #if ball.vel.x - plano_inclinado >= COEFICIENTE_ATRITO_ESTATICO:
+        #    ball.vel.x -= plano_inclinado
+        if terrain.onHole(ball.rect.x, ball.rect.y):
             # aqui é o evento para quando a bola entra no buraco
             print('Yay! Dentro do buraco!')
+            on_hole += 1
+
+        '''else:
+            #apenas para propositos de debug
+            x_div = terrain.getDiv(ball.rect.x)
+            print('X_div: {}, Y[div]: {}, Y[div + 1]: {}'.format(x_div, terrain.Y[x_div-1], terrain.Y[x_div]))'''
         while pygame.sprite.groupcollide(ball_group, terrain_group, False, False, pygame.sprite.collide_mask):
             on_sand = True
-            ball.rect.y -= 0.25
+            ball.rect.y -= 0.5
             if ball.vel.magnitude() <= 0.4:
                 #jogar a bola novamente
                 mouse_pos = pygame.mouse.get_pos()
@@ -88,19 +123,36 @@ while running:
                     traction = vec(K_FORCE * (mouse_pos - ball.pos).x, K_FORCE * (mouse_pos - ball.pos).y)
                     ball.vel += traction
                     on_sand = False
-                
-                    
+                    golf_hit.play()
+
             #ball.vel.y = 0
     else:
         on_sand = False
 
     if ball.rect.x < 0 or ball.rect.x > WIDTH:
-        ball_group.remove(ball)
-        ball = Ball()
-        ball_group.add(ball)
+        ball.pos = (terrain.X[1] + 10, 200)
+        ball.vel = vec(0, 0)
+        ball.acc = vec(0, 0)
+
+    if on_hole > 100:
+        ball.pos = (terrain.X[1] + 10, 200)
+        terrain_group.remove(terrain)
+        terrain = TerrainGenerator(WIDTH, HEIGHT, 8, (200, 320), mode)
+        terrain_group.add(terrain)
+        on_hole = 0
+        score += 1
 
     terrain_group.draw(screen)
+
+    if menu.firt:
+        menu.firt = False
+        running = menu.help(screen)
+
     ball_group.draw(screen)
+
+    # DESENHANDO A BANDEIRA
+    screen.blit(flag, (terrain.X[-3], terrain.Y[-3] - 20))
+    screen.blit(score_font.render(str(score), True, WHITE), (terrain.X[-3] + 5, terrain.Y[-3] - 18))
 
     mouse_old = mouse_curr
 
