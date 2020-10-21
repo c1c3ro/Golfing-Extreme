161d56fb306d2ecf5dcd68feaaf87fccbb3ea0ea
Thiago Carlos Silva Pereira <49200148+thcls@users.noreply.github.com>
2020-10-21 16:56:35 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,19 +1,28 @@
 import pygame
+from constants import *
+import math
 
 class Menu:
+
+    def __init__(self):
+        super().__init__()
+        self.x = 0
+        self.y = 0
+        self.firt = True
 
     def initial_menu(self, screen):
 
         (x, y) = (0, 0)
         clock = pygame.time.Clock()
 
+        anime = pygame.image.load('fx.png')
         ball = pygame.image.load('golf-ball8.png')
         startmenu = pygame.image.load("Initialmenu.jpg")
 
         screen.blit(startmenu, (0, 0))
 
         while True:
-            clock.tick(30)
+            clock.tick(6)
 
             for event in pygame.event.get():
 
@@ -38,4 +47,36 @@ class Menu:
                     if event.type == 5:
                         return False
 
+            pygame.display.update()
+
+    def help(self, screen):
+
+        while True:
+            text = "Utilize o mouse para escolher a direção"
+            text1 = "e aperte o botão esquerdo do mouse para"
+            text2 = "dar a tacada."
+            text3 = "Clique para continuar..."
+
+            window = pygame.image.load("window.png")
+
+            screen.blit(window, (20, 20))
+
+            font = pygame.font.SysFont(None, 30)
+            help = font.render(text, True, WHITE)
+
+            screen.blit(help, (50,50))
+            help = font.render(text1, True, WHITE)
+            screen.blit(help, (50, 70))
+
+            help = font.render(text2, True, WHITE)
+            screen.blit(help, (50, 90))
+
+            help = font.render(text3, True, WHITE)
+            screen.blit(help, (50, 120))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return False
+                if event.type == 5:
+                    return True
             pygame.display.update()
